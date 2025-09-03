03c4cee045f980ea8cce7ba528163cdb419845f8
Guqier <2535810484@qq.com>
2025-09-03 22:00:57 +0800

    fix(课程知识问答): 修复魔法包导入.

--- src/layout/components/sidebar/index.vue
+++ src/layout/components/sidebar/index.vue
@@ -1,11 +1,11 @@
 <template>
     <SideLogo />
     <SideMenu />
-    <SideChatSession />
+    <!-- <SideChatSession /> -->
 </template>
 
 <script lang="ts" setup>
 import SideLogo from "./components/SideLogo.vue";
 import SideMenu from "./components/SideMenu.vue";
-import SideChatSession from "./components/SideChatSession.vue";
+// import SideChatSession from "./components/SideChatSession.vue";
 </script>
